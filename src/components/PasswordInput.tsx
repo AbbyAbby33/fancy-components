1ff48a0cd48f09ab384a26550ba079165d99a4db
Abby <sk573129@gmail.com>
2022-07-03 23:46:43 +0800

    feat: 處理PasswordInput樣式

--- src/components/PasswordInput.tsx
+++ src/components/PasswordInput.tsx
@@ -1,6 +1,20 @@
 import TextField from '@mui/material/TextField';
+import { useState } from 'react';
 
 export default function PasswordInput() {
+
+    const [inputFocus, setInputFocus] = useState(false);
+
+    const onFocus = function () {
+        console.log('onFocus');
+        setInputFocus(true);
+    }
+
+    const onBlur = function () {
+        console.log('onBlur');
+        setInputFocus(false);
+    }
+
     return (
         <div>
             {/* 1.沒有focused版本 */}
@@ -11,8 +25,23 @@ export default function PasswordInput() {
                 sx={{
                     width: "335px",
                     height: "58px",
+                    // label color
+                    "& label": {
+                        color: "#fff",
+                    },
+                    // label color
+                    "& .Mui-focused": {
+                        color: "#fff !important",
+                    },
                     "& .MuiOutlinedInput-root": {
-                        color: "#fff"
+                        color: "#fff",
+                        '&.Mui-focused fieldset': {
+                            // borderColor: 'rgba(255, 255, 255, .5)',
+                            borderColor: inputFocus ? '#00A3FF !important' : 'rgba(255, 255, 255, .5)'
+                        },
+                        '&:hover fieldset': {
+                            borderColor: '#fff',
+                        },
                     }
                 }}
                 inputProps={{
@@ -21,7 +50,9 @@ export default function PasswordInput() {
                             color: "#fff"
                         }
                     }
-                }} />
+                }}
+                onFocus={onFocus}
+                onBlur={onBlur} />
         </div>
     )
 }
